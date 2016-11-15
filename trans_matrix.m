45aa4074aa2b12c275985807733cc07001b8a105
Rikharthu <UberViolence@gmail.com>
2016-11-15 20:24:59 +0200

    Initial commit

---
+++ trans_matrix.m
@@ -0,0 +1,8 @@
+function A = trans_matrix(O,l,d,a)
+A=[
+    cos(O),-sin(O)*cosd(a),sin(O)*sind(a),l*cos(O);
+    sin(O),cos(O)*cosd(a),-cos(O)*sind(a),l*sin(O);
+    0,sind(a),cosd(a),d;
+    0,0,0,1;
+    ];
+end
