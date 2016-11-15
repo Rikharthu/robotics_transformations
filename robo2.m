91b9080854d515233e30fc7590e07c99e69f091d
Rikharthu <uberviolence@gmail.com>
2016-11-15 20:30:50 +0200

    removed Rinalds from robo2

--- robo2.m
+++ robo2.m
@@ -26,21 +26,3 @@ A3=simplify(trans_matrix(O3,l3,d3,a3))
 
 T1=simplify(A2*A3)
 T2=simplify(A1*T1)
-%% Rinalds
-clc; clear all;
-syms O1 l1 d1 a1 O2 l2 d2 a2 O3 l3 d3 a3;
-
-l1=0;
-O1=0;
-a1=90;
-a2=0;
-a3=0;
-d2=0;
-d3=0;
-
-A1=simplify(trans_matrix(O1,l1,d1,a1))
-A2=simplify(trans_matrix(O2,l2,d2,a2))
-A3=simplify(trans_matrix(O3,l3,d3,a3))
-
-T1=simplify(A2*A3)
-T2=simplify(A1*T1)
