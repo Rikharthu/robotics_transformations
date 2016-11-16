8f09c09dc513e27ffb32d0089336b1c1076e5c43
Rikharthu <uberviolence@gmail.com>
2016-11-16 17:54:12 +0200

    renamed trans_matrix to get_matrix. added inverse matrix

--- robo2.m
+++ robo2.m
@@ -3,7 +3,7 @@ clc; clear all;
 syms O1 l1 d1 a1 O2 l2 d2 a2 O3 l3 d3 a3;
 
 % manipulatora parametri
-% do not touch
+% specifiskie parametri:
 l1=0;
 a1=-90;
 l2=0;
@@ -11,7 +11,7 @@ a2=-90;
 l3=0;
 a3=0;
 O3=0;
-% configure
+% mainamie parametri:
 %{
 O1=0;
 d1=0;
@@ -20,9 +20,13 @@ d2=0;
 d3=0;
 %}
 
-A1=simplify(trans_matrix(O1,l1,d1,a1))
-A2=simplify(trans_matrix(O2,l2,d2,a2))
-A3=simplify(trans_matrix(O3,l3,d3,a3))
+A1=simplify(get_matrix(O1,l1,d1,a1))
+A2=simplify(get_matrix(O2,l2,d2,a2))
+A3=simplify(get_matrix(O3,l3,d3,a3))
 
 T1=simplify(A2*A3)
 T2=simplify(A1*T1)
+
+A1_=simplify(inv(A1))
+A2_=simplify(inv(A2))
+A3_=simplify(inv(A3))
